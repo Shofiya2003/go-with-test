859d122539c6e207adcfd3aa6282ca430f6498da
Shofiya2003 <shofiyabootwala@gmail.com>
2022-12-29 14:51:32 +0530

    included subtests

--- hello.go
+++ hello.go
@@ -5,9 +5,12 @@ import "fmt"
 const englishHelloPrefix = "Hello "
 
 func Hello(name string) string {
+	if name == "" {
+		return englishHelloPrefix + "World"
+	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello(""))
 }
--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello Chris"
+	t.Run("saying hello to a person", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello Chris"
+		asserCorrectMessage(t, got, want)
+	})
+
+	t.Run("say `Hello World` when name is empty", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
+		asserCorrectMessage(t, got, want)
+	})
+
+}
+
+func asserCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
